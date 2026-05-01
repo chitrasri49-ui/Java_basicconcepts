7e7a63bccbf911da20886c5ad1d583ff1c9659ee
chitrasri49-ui <chitrasri49@gmail.com>
2026-05-01 20:30:26 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hellow world");
 System.out.println("Welcome to POllScm method initial");
+System.out.println("Welcome to POllScm method second time");
 }
 }
