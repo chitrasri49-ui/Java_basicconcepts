4131a788ed70791059b5bfa10a7c84dcab1bd35a
chitrasri49-ui <chitrasri49@gmail.com>
2026-05-01 20:25:57 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,6 +2,6 @@ public class jenkins {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hellow world");
-System.out.println("Welcome to POllScm method first time");
+System.out.println("Welcome to POllScm method initial");
 }
 }
