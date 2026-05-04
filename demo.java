13c570c3c2630277265bf8eea106631916f320d9
chitrasri49-ui <chitrasri49@gmail.com>
2026-05-04 11:39:51 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,10 +1,6 @@
 public class demo {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellow world");
-System.out.println("Welcome to POllScm method initial");
-System.out.println("Welcome to POllScm method second time");
-	System.out.println("Welcome to pipeline job running");
-
+System.out.println("Hello from jenkins java Pipeline");
 }
 }
